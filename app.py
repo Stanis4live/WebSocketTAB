da56c3ec74e6374298bd2cb9274b8982294e373f
stas <chousetheone@gmail.com>
2024-11-17 11:59:46 +0200

    2

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class ConnectionManager:
     def __init__(self):
